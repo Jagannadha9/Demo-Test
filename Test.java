629e0ca4b63c106072d8170cf0bf583837ee34f0
A07076_h4.02.13 <A07076_h4.02.13@accenture.com>
2018-09-04 12:59:29 +0530

    adding the for loop

--- Test.java
+++ Test.java
@@ -2,8 +2,10 @@
 public class Test {
 
 	public static void main(String[] args) {
+	for(int i=0; i<10; i++)
+	
 		// TODO Auto-generated method stub
-		System.out.println("This is test program");
+		System.out.println("This is test program" + i);
 	}
 
 }
